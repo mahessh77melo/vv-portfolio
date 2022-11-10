18d747d565687562e173d9cf944afe3d1dc8733d
Magesh J <magesh.lbj23@gmail.com>
2022-11-10 10:56:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="title">
+					<div className="title__name">Vishnu Vardan</div>
+					<div className="title__role">
+						{" "}
+						- Programmer <span class="or-symbol">|</span> Game Dev{" "}
+						<span class="or-symbol">|</span> Guitarist
+					</div>
+				</div>
+				<div className="image-links">
+					<div className="img img--1">
+						<img
+							src="https://besthqwallpapers.com/Uploads/2-1-2021/150010/thumb2-github-red-logo-4k-red-brickwall-github-logo-social-networks.jpg"
+							alt="github logo"
+							className="image"
+						/>
+					</div>
+					<div className="img img--2">
+						<img
+							src="https://kiiky.com/wp-content/uploads/2020/06/CV-boost.jpg"
+							alt="cv padam"
+							className="image"
+						/>
+					</div>
+					<div className="img img--3">
+						<img
+							src="https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+							alt="projects text"
+							className="image"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
