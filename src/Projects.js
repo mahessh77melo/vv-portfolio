53b45a559404109f1b45e44403357e5958fe0625
Magesh J <magesh.lbj23@gmail.com>
2022-11-19 10:22:00 +0530

    projects - json

--- src/Projects.js
+++ src/Projects.js
@@ -1,32 +1,23 @@
 import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import WebIcon from "@material-ui/icons/Web";
+import projectsJson from "./projects.json";
 import "./Projects.scss";
 
 const Projects = () => {
-	const projectsArray = [
-		{
-			title: "Lebron James Analysis",
-			domain: "Data Analysis",
-			tools: ["Python", "pandas", "matplotLib", "jupyter"],
-			github: "https://github.com/mahessh77melo/kingjames",
-		},
-		{
-			title: "Natours",
-			domain: "Web Design",
-			tools: ["CSS", "SCSS"],
-			github: "https://github.com/mahessh77melo/Natours-SCSS",
-		},
-	];
+	const projectsArray = projectsJson || [];
+	console.log(projectsArray);
 	return (
 		<div className="projects">
 			{projectsArray.map((pro) => (
-				<div className="project">
+				<div className="project" key={projectsArray.title}>
 					<div className="project__title">{pro.title}</div>
 					<div className="project__domain">{pro.domain}</div>
 					<div className="project__tools">
 						{pro.tools?.map((tool) => (
-							<div className="project__tools-item">{tool}</div>
+							<div className="project__tools-item" key={tool}>
+								{tool}
+							</div>
 						))}
 					</div>
 					<div className="project__links">
